53f7fe616e5cc280f3079f1ee089c46b2670b079
Mark Hollas <markhollas@Marks-MacBook-Air.local>
2026-05-17 13:53:49 +0900

    added count down timer, added effects

--- PomniMaze/js/index.js
+++ PomniMaze/js/index.js
@@ -5,24 +5,56 @@ let letsGoButton = document.querySelector("#lets-go-button");
 
 let doIt = document.querySelector("#do-it");
 let doItTimes = document.querySelector("#do-it-times");
+let pomni = document.querySelector("#pomni-logo2");
+let caine = document.querySelector("#caine-logo");
+let timer = document.querySelector("#timer");
 
 let = doItText = "";
 let = doItTimesText = "";
+let = timerTime = 30;
+let timerId = 0;
+
+timer.textContent = timerTime;
 
 resetButton.addEventListener('click', (button) => {
     doIt.textContent = "";
-    doItTimes.textContent = "";
+    doItTimes.textContent = ""
     doItText = "";
     doItTimesText = "";
+    pomni.src = "images/pomni_02.png";
+    timer.textContent = timerTime;
+    stopTimer();
 });
 
 letsGoButton.addEventListener('click', (button) => {
+    if (doItText == "" && doItTimesText == "") return;
     doIt.textContent = doItText;
     doItTimes.textContent = doItTimesText;
+    pomni.src = "images/pomni_01.png";
+    startTimer(timerTime);
 });
 
+caine.addEventListener('click', (button) => {
+    stopTimer();
+})
+
+function startTimer(time) {
+    console.log("GO!")
+    timer.textContent = time--;
+    timerId = setInterval(() => {
+        timer.textContent = time--;
+        if (time < 0) {
+            stopTimer();
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+}
 
 function pomniDoSomething(doThis, times) {
     doItText = doThis;
     doItTimesText = times;
+    console.log("Command is ready. Hit 'Lets Go!'");
 }
